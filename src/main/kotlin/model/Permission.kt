195eff2b8add3d20d7c0fdbe6164a419e4979a79
= <aalma@mail.austral.edu.ar>
2024-10-02 14:03:50 -0300

    updated permission model

--- src/main/kotlin/model/Permission.kt
+++ src/main/kotlin/model/Permission.kt
@@ -4,16 +4,26 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.jetbrains.annotations.NotNull
 
 @Entity
-data class Snippet(
+data class Permission(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     val id: Int? = null,
+    @NotNull
     val userId: String,
+    @NotNull
     val snippetId: String,
+    @NotNull
     val read: Boolean,
+    @NotNull
     val write: Boolean,
+    @NotNull
     val execute: Boolean,
+    @NotNull
     val share: Boolean
+
+    // convertir rwxs en una clase q los maneje y cambiarle eno nombre a permissionInfo o similar
 )
