f90b4b6c69bfc8ed71ec07b2cbd9e362571a57b9
= <aalma@mail.austral.edu.ar>
2024-10-14 09:41:16 -0300

    fixed controller

--- src/main/kotlin/app/controllers/PermissionController.kt
+++ src/main/kotlin/app/controllers/PermissionController.kt
@@ -5,9 +5,12 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
-@Controller("api")
+@RestController
+@RequestMapping("api")
 class PermissionController(private val permissionService: PermissionService) {
 
     @GetMapping("get")
