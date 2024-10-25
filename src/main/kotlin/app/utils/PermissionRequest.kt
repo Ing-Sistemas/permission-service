a5ba2ba74b821d23809338413ed1c6a01ff958ee
agussoul <aalma@mail.austral.edu.ar>
2024-10-24 23:54:36 -0300

    rm import

--- src/main/kotlin/app/utils/PermissionRequest.kt
+++ src/main/kotlin/app/utils/PermissionRequest.kt
@@ -1,7 +1,5 @@
 package com.example.springboot.app.utils
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt
-
 data class PermissionRequest(
     val snippetId: String,
 )
